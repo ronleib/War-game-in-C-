ccacf5e199740fd31369a40f026cf896675d1c5d
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-05-24 20:17:44 +0300

    Add files via upload

--- Soldier.cpp
+++ Soldier.cpp
@@ -3,30 +3,6 @@
 #include "Soldier.hpp"
 #include "Board.hpp"
 using namespace WarGame;
-//// move functins
-void Soldiers::FootSoldier::move( WarGame::Board::MoveDIR dir ){
-
-}
-
-Soldiers::FootCommander::void move( MoveDIR dir ){
-
-}
-
-Soldiers::Sniper::void move( MoveDIR dir ){
-
-}
-
-Soldiers::SniperCommander::void move( MoveDIR dir ){
-
-}
-
-Soldiers::Paramedic::void move( MoveDIR dir ){
-
-}
-
-Soldiers::ParamedicCommander::void move( MoveDIR dir ){
-
-}
 
 //// spetialAbility functins
 Soldiers::FootCommander::void spetialAbility(){
