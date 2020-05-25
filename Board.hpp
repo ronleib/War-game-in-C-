17c920f8a1034d4ccc6cdd03b050bfc55a590b1b
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-05-25 23:42:27 +0300

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -45,6 +45,8 @@ class Board {
 
     // returns true iff the board contains one or more soldiers of the given player.
     bool has_soldiers(uint player_number) const;
+  
+    void clear();
 };
 
 }
